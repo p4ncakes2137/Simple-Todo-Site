2b0e1b7c5c65a2edc8c4b41df472db5f766c9588
p4ncake_s <wojeciechpawlowicz99@gmail.com>
2025-02-27 22:21:40 +0100

    added small comment

--- frontend/src/scripts/config.js
+++ frontend/src/scripts/config.js
@@ -1,6 +1,6 @@
 let token = localStorage.getItem('token') || null;
 
-export const API_URL = "http://localhost:3000/api";
+export const API_URL = "http://localhost:3000/api"; // use /api in production code
 export const getToken = () => token;
 export const setToken = (newToken) => {
   token = newToken;
